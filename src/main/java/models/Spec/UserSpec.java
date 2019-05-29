eb6a66de7734b931ff579ee8705b58da0f8a9b62
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-05-29 23:14:44 +0300

    userspec model added

---
+++ src/main/java/models/Spec/UserSpec.java
@@ -0,0 +1,46 @@
+package models.Spec;
+
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UserSpec {
+
+    @NotNull
+    @Size(min=7, max=22, message="Name should be be between 7 and 18 char.")
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String repeatPassword;
+
+    public UserSpec() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+}
