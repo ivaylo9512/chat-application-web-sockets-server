b976ad29fe6473d70cce3a1ec5d1bdf536f78930
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-05 23:37:45 +0300

    message repository

---
+++ src/main/java/repositories/base/MessageRepository.java
@@ -0,0 +1,15 @@
+package repositories.base;
+
+import models.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    @Query(value="from Message where receiverId = :userId and session_date = :lastCheckDate and time > :lastCheckTime or receiverId = :userId and session_date > :lastCheckDate")
+    List<Message> findMostRecentMessages(@Param("userId") int userId, @Param("lastCheckDate") LocalDate lastCheckDate, @Param("lastCheckTime") LocalTime lastCheckTime);
+}
