6452777bd4f9f0e9c859408a77bb7e00d3713d88
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2019-06-06 06:58:23 +0300

    user controller added

---
+++ src/main/java/com/chat/app/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.chat.app.controllers;
+
+
+import com.chat.app.exceptions.UsernameExistsException;
+import com.chat.app.models.DTOs.UserDto;
+import com.chat.app.models.UserDetails;
+import com.chat.app.models.specs.UserSpec;
+import com.chat.app.services.base.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping(value = "/auth/users/adminRegistration")
+    public UserDto registerAdmin(@Valid UserSpec user){
+        return new UserDto(userService.register(user,"ROLE_USER"));
+    }
+
+
+    @GetMapping(value = "/findById/{id}")
+    public UserDto findById(@PathVariable(name = "id") int id){
+        return new UserDto(userService.findById(id));
+    }
+
+    @PostMapping(value = "/changeUserInfo")
+    public UserDto changeUserInfo(@RequestBody UserSpec userModel){
+        UserDetails loggedUser = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication().getDetails();
+
+        return new UserDto(userService.changeUserInfo(loggedUser.getId(), userModel));
+    }
+    @ExceptionHandler
+    ResponseEntity handleUsernameExistsException(UsernameExistsException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(e.getMessage());
+    }
+    
+}
