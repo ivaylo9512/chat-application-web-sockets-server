cc2fcf05df9966fc2b911dc17eb8a6fcebb18d8a
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-08-16 21:42:20 +0300

    ChatDto change - sessions to dto + first and second user

--- src/main/java/com/chat/app/models/DTOs/ChatDto.java
+++ src/main/java/com/chat/app/models/DTOs/ChatDto.java
@@ -1,34 +1,39 @@
 package com.chat.app.models.DTOs;
 
 import com.chat.app.models.Chat;
-import com.chat.app.models.Session;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChatDto {
     private int id;
-    private UserDto user;
-    private List<Session> sessions;
+    private UserDto firstUser;
+    private UserDto secondUser;
+    private List<SessionDto> sessions;
 
     public ChatDto() {
     }
 
     public ChatDto(Chat chat) {
         this.id = chat.getId();
-        this.sessions = chat.getSessions();
+        this.firstUser = new UserDto(chat.getFirstUserModel());
+        this.secondUser = new UserDto(chat.getSecondUserModel());
+        this.sessions = chat.getSessions().stream().map(SessionDto::new).collect(Collectors.toList());
     }
 
-    public ChatDto(Chat chat, UserDto user) {
-        this.id = chat.getId();
-        this.sessions = chat.getSessions();
-        this.user = user;
+    public UserDto getFirstUser() {
+        return firstUser;
     }
 
-    public UserDto getUser() {
-        return user;
+    public void setFirstUser(UserDto firstUser) {
+        this.firstUser = firstUser;
     }
 
-    public void setUser(UserDto user) {
-        this.user = user;
+    public UserDto getSecondUser() {
+        return secondUser;
+    }
+
+    public void setSecondUser(UserDto secondUser) {
+        this.secondUser = secondUser;
     }
 
     public int getId() {
@@ -39,11 +44,11 @@ public class ChatDto {
         this.id = id;
     }
 
-    public List<Session> getSessions() {
+    public List<SessionDto> getSessions() {
         return sessions;
     }
 
-    public void setSessions(List<Session> sessions) {
+    public void setSessions(List<SessionDto> sessions) {
         this.sessions = sessions;
     }
 }
