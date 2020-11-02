5b39a3d420b52862709a466bef32bd178b159e3a
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-11-02 22:40:28 +0200

    RegisterSpec

---
+++ src/main/java/com/chat/app/models/specs/RegisterSpec.java
@@ -0,0 +1,35 @@
+package com.chat.app.models.specs;
+
+public class RegisterSpec {
+    private String username;
+    private String password;
+    private String repeatPassword;
+
+    public RegisterSpec() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+}
