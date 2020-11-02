e6c9d819cec849d2fde50b3a8ae65bc631b79226
ivailo aleksandrov <ivayloaleksandrov@yahoo.com>
2020-11-02 22:41:55 +0200

    NewPasswordSpec

---
+++ src/main/java/com/chat/app/models/specs/NewPasswordSpec.java
@@ -0,0 +1,40 @@
+package com.chat.app.models.specs;
+
+public class NewPasswordSpec {
+    private String username;
+    private String currentPassword;
+    private String newPassword;
+    private String repeatNewPassword;
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getRepeatNewPassword() {
+        return repeatNewPassword;
+    }
+
+    public void setRepeatNewPassword(String repeatNewPassword) {
+        this.repeatNewPassword = repeatNewPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
