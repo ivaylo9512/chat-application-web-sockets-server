c3a68c39eb8b446ac540c511af707e0014338762
ivaylo <ivayloaleksandrov@yahoo.com>
2020-11-04 23:53:15 +0200

    File model added

---
+++ src/main/java/com/chat/app/models/File.java
@@ -0,0 +1,75 @@
+package com.chat.app.models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id", scope = File.class)
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String type;
+    private double size;
+
+    public File() {
+
+    }
+
+    public File(String name, double size, String type){
+        this.name = name;
+        this.size = size;
+        this.type = type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Long.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) return true;
+        if(!(obj instanceof File)) return false;
+
+        File extension = (File) obj;
+        return extension.getId() == getId();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
