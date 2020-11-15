b5f8bf8ee3e405df4e5cacfc3a71cef38a2bc9fb
ivaylo <ivayloaleksandrov@yahoo.com>
2020-11-15 22:26:21 +0200

    FileService create + update added

--- src/main/java/com/chat/app/services/FileServiceImpl.java
+++ src/main/java/com/chat/app/services/FileServiceImpl.java
@@ -1,14 +1,21 @@
 package com.chat.app.services;
 
 import com.chat.app.exceptions.FileNotFoundUncheckedException;
+import com.chat.app.exceptions.FileStorageException;
 import com.chat.app.services.base.FileService;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -26,7 +33,33 @@ public class FileServiceImpl implements FileService {
     }
 
     @Override
-    public Resource loadFileAsResource(String fileName) throws FileNotFoundUncheckedException {
+    public File update(MultipartFile file, String name, long id){
+        File updatedFile = create(file, name);
+        updatedFile.setId(id);
+        return fileRepository.save(updatedFile);
+    }
+
+    @Override
+    public File create(MultipartFile receivedFile, String name) {
+
+        File file = generate(receivedFile, name);
+
+        try {
+            if (!file.getType().startsWith("image/")) {
+                throw new FileFormatException("File should be of type IMAGE.");
+            }
+
+            save(file, receivedFile);
+
+            return file;
+
+        } catch (IOException e) {
+            throw new FileStorageException("Couldn't store the image.");
+        }
+    }
+
+    @Override
+    public Resource getFileAsResource(String fileName){
         try {
             Path filePath = this.fileLocation.resolve(fileName).normalize();
             Resource resource = new UrlResource(filePath.toUri());
@@ -38,5 +71,16 @@ public class FileServiceImpl implements FileService {
         } catch (MalformedURLException e) {
             throw new FileNotFoundUncheckedException("File not found " + e);
         }
+    }
+
+    private void save(File image, MultipartFile receivedFile) throws IOException {
+        Path targetLocation = this.fileLocation.resolve(image.getName());
+        Files.copy(receivedFile.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private File generate(MultipartFile receivedFile, String name) {
+        String fileType = FilenameUtils.getExtension(receivedFile.getOriginalFilename());
+        String fileName = name + "." + fileType;
+        return new File(fileName, receivedFile.getSize(), receivedFile.getContentType());
     }
 }
--- src/main/java/com/chat/app/services/base/FileService.java
+++ src/main/java/com/chat/app/services/base/FileService.java
@@ -1,8 +1,15 @@
 package com.chat.app.services.base;
 
 import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public interface FileService {
-    Resource loadFileAsResource(String fileName) throws FileNotFoundException;
+    Resource getFileAsResource(String fileName) throws FileNotFoundException;
+
+    File update(MultipartFile file, String name, long id);
+
+    File create(MultipartFile receivedFile, String name);
 }
