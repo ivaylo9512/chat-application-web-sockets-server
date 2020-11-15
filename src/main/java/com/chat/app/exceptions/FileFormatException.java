fb8106c1517aefdcdf287aa4a2d779b11916f695
ivaylo <ivayloaleksandrov@yahoo.com>
2020-11-15 22:29:11 +0200

    FileFormatException + FileRepository added

---
+++ src/main/java/com/chat/app/exceptions/FileFormatException.java
@@ -0,0 +1,9 @@
+package com.chat.app.exceptions;
+
+public class FileFormatException extends RuntimeException {
+
+    public FileFormatException(String exception) {
+        super(exception);
+    }
+
+}
