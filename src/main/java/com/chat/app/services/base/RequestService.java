cb8c9c80f79e697a957156e3c8c034a080ff517c
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-12 20:38:36 +0300

    RequestService added

--- src/main/java/com/chat/app/services/base/RequestService.java
+++ src/main/java/com/chat/app/services/base/RequestService.java
@@ -1,5 +1,15 @@
 package com.chat.app.services.base;
 
+import com.chat.app.models.Request;
+import org.springframework.data.domain.Page;
+
 public interface RequestService {
 
+    Page<Request> findAll(Long userId, int pageSize, String lastCreatedAt, long lastId);
+
+    Request findByUsers(long firstUser, long secondUser);
+
+    void delete(long id);
+
+    Request create(long from, long to);
 }
