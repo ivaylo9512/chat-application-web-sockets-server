b11ef5c15d61578db91bbc1c3ae19bf5b35c2144
ivaylo <ivayloaleksandrov@yahoo.com>
2021-08-26 00:35:17 +0300

    User integration tests setup change + content return changes

--- src/test/java/integration/Users.java
+++ src/test/java/integration/Users.java
@@ -2,21 +2,23 @@ package integration;
 
 import com.chat.app.config.AppConfig;
 import com.chat.app.config.SecurityConfig;
+import com.chat.app.config.TestDataSourceConfig;
 import com.chat.app.config.TestWebConfig;
 import com.chat.app.controllers.UserController;
+import com.chat.app.models.Dtos.UserDto;
 import com.chat.app.models.UserDetails;
 import com.chat.app.models.UserModel;
 import com.chat.app.security.Jwt;
-import com.chat.app.services.base.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -26,9 +28,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import javax.servlet.ServletContext;
+import javax.sql.DataSource;
 import java.util.Collections;
 
 import static org.hamcrest.Matchers.containsString;
@@ -36,9 +40,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { AppConfig.class, TestWebConfig.class, SecurityConfig.class})
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(classes = { AppConfig.class, TestWebConfig.class, SecurityConfig.class, TestDataSourceConfig.class})
 @WebAppConfiguration(value = "src/main/java/com/chat/app")
 @WebMvcTest(controllers = UserController.class)
 @Import(SecurityConfig.class)
@@ -47,26 +53,31 @@ public class Users {
     private WebApplicationContext webApplicationContext;
 
     @Autowired
-    private UserService userService;
+    private DataSource dataSource;
 
     private MockMvc mockMvc;
-
-    public static UserModel userDetails;
-    public static String adminToken;
+    private static String adminToken;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
+    public void setupData() {
+        ResourceDatabasePopulator rdp = new ResourceDatabasePopulator();
+        rdp.addScript(new ClassPathResource("integrationTestsSql/UsersData.sql"));
+        rdp.execute(dataSource);
+    }
+
+    @BeforeAll
     public void setup() {
+        UserModel admin = new UserModel("adminUser", "password", "ROLE_ADMIN");
+        admin.setId(1);
+        adminToken = "Token " + Jwt.generate(new UserDetails(admin, Collections
+                .singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+
+        objectMapper = new ObjectMapper();
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(webApplicationContext)
                 .apply(SecurityMockMvcConfigurers.springSecurity())
                 .build();
-    }
-
-    @BeforeAll
-    public void setupUser() {
-        userDetails = userService.create(new UserModel("admin123", "admin", "ROLE_ADMIN"));
-        adminToken = "Token " + Jwt.generate(new UserDetails(userDetails, Collections
-                .singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
     }
 
     @Test
@@ -78,35 +89,49 @@ public class Users {
         Assert.assertNotNull(webApplicationContext.getBean("userController"));
     }
 
+    private UserModel user = new UserModel("username", "password","ROLE_USER", "firstname",
+            "lastname", 25, "Bulgaria");
+    private UserDto userDto = new UserDto(user);
+
+    private RequestBuilder createMediaRegisterRequest(String url, String role, String token){
+        RequestBuilder request = post(url)
+                .param("username", user.getUsername())
+                .param("password", user.getPassword())
+                .param("repeatPassword", user.getPassword())
+                .param("firstName", user.getFirstName())
+                .param("lastName", user.getLastName())
+                .param("age", String.valueOf(user.getAge()))
+                .param("country", user.getCountry());
+
+        if(token != null){
+            ((MockHttpServletRequestBuilder) request).header("Authorization", token);
+        }
+        userDto.setRole(role);
+        userDto.setId(8);
+
+        return  request;
+    }
+
     @WithMockUser(value = "spring")
     @Test
     public void register() throws Exception {
-        RequestBuilder request = post("/api/users/register")
-                .param("username", "username")
-                .param("password", "password")
-                .param("repeatPassword", "password");
-
-        mockMvc.perform(request)
-                .andExpect(status().isOk());
+        mockMvc.perform(createMediaRegisterRequest("/api/users/register", "ROLE_USER", null))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(userDto))));
     }
 
     @WithMockUser(value = "spring")
     @Test
     public void registerAdmin() throws Exception {
-        RequestBuilder request = post("/api/users/auth/registerAdmin")
-                .header("Authorization", adminToken)
-                .param("username", "username1")
-                .param("password", "password")
-                .param("repeatPassword", "password");
-
-        mockMvc.perform(request)
-                .andExpect(status().isOk());
+        mockMvc.perform(createMediaRegisterRequest("/api/users/auth/registerAdmin", "ROLE_ADMIN", adminToken))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(objectMapper.writeValueAsString(userDto))));
     }
 
     @Test
     public void register_WhenUsernameIsTaken() throws Exception {
         RequestBuilder request = post("/api/users/register")
-                .param("username", "username")
+                .param("username", "testUser")
                 .param("password", "password")
                 .param("repeatPassword", "password");
 
@@ -118,7 +143,8 @@ public class Users {
     public void login() throws Exception {
         mockMvc.perform(post("/api/users/login")
             .contentType("Application/json")
-            .content("{\"username\": \"username\", \"password\": \"password\"}")).andExpect(status().isOk());
+            .content("{\"username\": \"adminUser\", \"password\": \"password\"}"))
+            .andExpect(status().isOk());
     }
 
     @Test
