3d86d9568157bfed789e78193b640779a4733d7b
ivaylo9512 <ivayloaleksandrov@yahoo.com>
2021-10-03 23:56:54 +0300

    EmailToken + RegistrationEvent added + database url changed

---
+++ src/main/java/com/chat/app/models/EmailToken.java
@@ -0,0 +1,62 @@
+package com.chat.app.models;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "email_tokens")
+public class EmailToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private UserModel user;
+
+    public EmailToken() {
+    }
+
+    private LocalDateTime expiryDate;
+
+    public EmailToken(String token, UserModel user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public UserModel getUser() {
+        return user;
+    }
+
+    public void setUser(UserModel user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDateTime expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
